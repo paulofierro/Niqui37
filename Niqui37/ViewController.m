72ff546ceb372219e4ffc56ecc6438a0e4023187
Paulo Fierro <paulo@paulofierro.com>
2016-02-18 12:52:28 -0500

    Added fireworks

--- Niqui37/ViewController.m
+++ Niqui37/ViewController.m
@@ -6,6 +6,8 @@
 //  Copyright © 2016 Paulo Fierro. All rights reserved.
 //
 
+@import QuartzCore;
+
 #import "ViewController.h"
 
 static NSString *birthdayString = @"18/02/2016";
@@ -19,6 +21,10 @@ static NSString *birthdayString = @"18/02/2016";
 @property (nonatomic, strong) IBOutlet UILabel *hours;
 @property (nonatomic, strong) IBOutlet UILabel *minutes;
 @property (nonatomic, strong) IBOutlet UILabel *seconds;
+@property (nonatomic, strong) IBOutlet UIView *fireworksView;
+@property (nonatomic, strong) CALayer *baseLayer;
+@property (nonatomic, strong) CAEmitterLayer *emitter;
+@property (nonatomic, getter=isUnlocked) BOOL unlocked;
 
 @end
 
@@ -28,7 +34,14 @@ static NSString *birthdayString = @"18/02/2016";
 {
     [super viewDidLoad];
     
+    // Add the emitter and add it to the view
+    [self.baseLayer addSublayer:self.emitter];
+    [self.fireworksView.layer addSublayer:self.baseLayer];
+    
+    // Update the date
     [self updateDate];
+
+    // Create the timer
     self.timer = [NSTimer scheduledTimerWithTimeInterval:0.5 target:self selector:@selector(updateDate) userInfo:nil repeats:YES];
 }
 
@@ -44,6 +57,8 @@ static NSString *birthdayString = @"18/02/2016";
         self.hours.text     = @"";
         self.minutes.text   = @"🎊🎂🎉";
         self.seconds.text   = @"";
+        
+        self.unlocked = YES;
     }
     else
     {
@@ -64,7 +79,116 @@ static NSString *birthdayString = @"18/02/2016";
     }
 }
 
+- (void)addFireworkAtPoint:(CGPoint)point
+{
+    UIImage *image = [UIImage imageNamed:@"firework"];
+    
+    CGRect bounds           = self.fireworksView.bounds;
+    CGPoint maxPoint        = CGPointMake(CGRectGetMaxX(bounds), CGRectGetMaxY(bounds));
+    CGPoint newOriginPoint  = CGPointMake(maxPoint.x - maxPoint.x/2, maxPoint.y - maxPoint.y/2);
+    CGPoint position        = CGPointMake(newOriginPoint.x + point.x, newOriginPoint.y + point.y);
+    
+    self.emitter.emitterPosition = position;
+
+    // Invisible particle representing the rocket before the explosion
+    CAEmitterCell *rocket       = [CAEmitterCell emitterCell];
+    rocket.name                 = @"rocket"; // Name the cell so that it can be animated later using keypath
+    rocket.emissionLongitude    = M_PI / 2;
+    rocket.emissionLatitude     = 0;
+    rocket.lifetime             = 1.6;
+    rocket.birthRate            = 3;
+    rocket.velocity             = 40;
+    rocket.velocityRange        = 100;
+    rocket.yAcceleration        = -250;
+    rocket.emissionRange        = M_PI / 4;
+    rocket.color                = [[UIColor whiteColor] colorWithAlphaComponent:0.5].CGColor;
+    rocket.redRange             = 0.5;
+    rocket.greenRange           = 0.5;
+    rocket.blueRange            = 0.5;
+    
+    // Flare particles emitted from the rocket as it flys
+    CAEmitterCell *flare    = [CAEmitterCell emitterCell];
+    flare.contents          = (id)image.CGImage;
+    flare.emissionLongitude = (4 * M_PI) / 2;
+    flare.scale             = 0.4;
+    flare.velocity          = 100;
+    flare.birthRate         = 45;
+    flare.lifetime          = 1.5;
+    flare.yAcceleration     = -350;
+    flare.emissionRange     = M_PI / 7;
+    flare.alphaSpeed        = -0.7;
+    flare.scaleSpeed        = -0.1;
+    flare.scaleRange        = 0.1;
+    flare.beginTime         = 0.01;
+    flare.duration          = 0.7;
+    
+    // The particles that make up the explosion
+    CAEmitterCell *firework = [CAEmitterCell emitterCell];
+    firework.name           = @"firework";
+    firework.contents       = (id)image.CGImage;
+    firework.birthRate      = 9999;
+    firework.scale          = 0.6;
+    firework.velocity       = 130;
+    firework.lifetime       = 2;
+    firework.alphaSpeed     = -0.2;
+    firework.yAcceleration  = -80;
+    firework.beginTime      = 1.5;
+    firework.duration       = 0.1;
+    firework.emissionRange  = 2 * M_PI;
+    firework.scaleSpeed     = -0.1;
+    firework.spin           = 2;
+    
+    // preSpark is an invisible particle used to later emit the spark
+    CAEmitterCell *preSpark = [CAEmitterCell emitterCell];
+    preSpark.name           = @"preSpark";
+    preSpark.birthRate      = 80;
+    preSpark.velocity       = firework.velocity * 0.70;
+    preSpark.lifetime       = 1.7;
+    preSpark.yAcceleration  = firework.yAcceleration * 0.85;
+    preSpark.beginTime      = firework.beginTime - 0.2;
+    preSpark.emissionRange  = firework.emissionRange;
+    preSpark.greenSpeed     = 100;
+    preSpark.blueSpeed      = 100;
+    preSpark.redSpeed       = 100;
+    
+    // The 'sparkle' at the end of a firework
+    CAEmitterCell *spark    = [CAEmitterCell emitterCell];
+    spark.contents          = (id)image.CGImage;
+    spark.lifetime          = 0.05;
+    spark.yAcceleration     = -250;
+    spark.beginTime         = 0.8;
+    spark.scale             = 0.4;
+    spark.birthRate         = 10;
+    
+    preSpark.emitterCells       = @[spark];
+    rocket.emitterCells         = @[flare, firework, preSpark];
+    self.emitter.emitterCells   = @[rocket];
+    
+    [self.fireworksView setNeedsDisplay];
+}
+
 #pragma mark - Getters
+
+- (CAEmitterLayer *)emitter
+{
+    if (_emitter == nil)
+    {
+        _emitter = [CAEmitterLayer layer];
+        _emitter.renderMode = kCAEmitterLayerBackToFront;
+    }
+    return _emitter;
+}
+
+- (CALayer *)baseLayer
+{
+    if (_baseLayer == nil)
+    {
+        _baseLayer = [CALayer layer];
+        _baseLayer.bounds = self.fireworksView.bounds;
+        _baseLayer.backgroundColor = self.fireworksView.backgroundColor.CGColor;
+    }
+    return _baseLayer;
+}
 
 - (NSDate *)birthday
 {
@@ -83,6 +207,17 @@ static NSString *birthdayString = @"18/02/2016";
         _dateFormatter.dateFormat = @"dd/MM/yyyy";
     }
     return _dateFormatter;
+}
+
+#pragma mark - Touches
+
+- (void)touchesEnded:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+{
+    if (self.isUnlocked)
+    {
+        UITouch *touch = touches.allObjects.firstObject;
+        [self addFireworkAtPoint:[touch locationInView:self.fireworksView]];
+    }
 }
 
 - (void)didReceiveMemoryWarning
