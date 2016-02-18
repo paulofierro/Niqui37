029f65ee35b67d614600a51e398698df2d2edd32
Paulo Fierro <paulo@paulofierro.com>
2016-02-18 17:49:02 -0500

    Added ticket VC

---
+++ Niqui37/TicketViewController.h
@@ -0,0 +1,13 @@
+//
+//  TicketViewController.h
+//  Niqui37
+//
+//  Created by Paulo Fierro on 18/02/2016.
+//  Copyright © 2016 Paulo Fierro. All rights reserved.
+//
+
+@import UIKit;
+
+@interface TicketViewController : UIViewController
+
+@end
---
+++ Niqui37/TicketViewController.m
@@ -0,0 +1,27 @@
+//
+//  TicketViewController.m
+//  Niqui37
+//
+//  Created by Paulo Fierro on 18/02/2016.
+//  Copyright © 2016 Paulo Fierro. All rights reserved.
+//
+
+#import "TicketViewController.h"
+
+@interface TicketViewController ()
+
+@end
+
+@implementation TicketViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+}
+
+@end
